23717cdbf77ea3d2170f28334846624109cdca88
Micheal Colhoun <micheal@cts-ni.com>
2021-09-09 16:26:09 +0100

    Add project files.

---
+++ FO.Model/Friend.cs
@@ -0,0 +1,12 @@
+﻿namespace FO.Model
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; private set; }
+        public string Email { get; set; }
+
+
+    }
+}
