c3b5c48dd58bc6c948655b7a0bb7f3f3c34b6c1a
Micheal Colhoun <micheal@cts-ni.com>
2021-09-10 09:10:09 +0100

    Fix Bugs, cleanup comments

--- FO.UI/App.xaml.cs
+++ FO.UI/App.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using FO.UI.Data;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace FO.UI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         private void Application_Startup(object sender, StartupEventArgs e)
--- FO.UI/MainWindow.xaml.cs
+++ FO.UI/MainWindow.xaml.cs
@@ -1,34 +1,18 @@
 ﻿using FO.UI.ViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FO.UI
 {
-   
+
     public partial class MainWindow : Window
     {
         private MainViewModel _vm;
 
         public MainWindow(MainViewModel vm)
         {
-            
             InitializeComponent();
             _vm = vm;
             DataContext = _vm;
-            //DataContext = vm;
-            //vm.Load(); don't do this.
             Loaded += MainWindow_Loaded;
         }
 
